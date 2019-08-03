c865afd3a996629170c2a8b3fb6475030cad550b
Lrop <Lagropner04@gmail.com>
2019-08-02 18:02:30 -0600

    saving again/bug error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,31 +3,47 @@ var inquirer = require('inquirer');
 var Table = require('cli-table2');
 
 var connection = mysql.createConnection({
-     host: "localhost",
-     user:"root",
-     password: "password",
-     database: "bamazon_db",
-     port: 3306
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "bamazon_db",
+  port: 3306
 
 });
 
 
-connection.connect(function(err){
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("CONNECTED" + connection.threadId);
+  // console.log("CONNECTED" + connection.threadId);
 });
 
 
-const displayItems = function() {
-  connection.query("SELECT * FROM products", function(err, res) {
+const displayItems = function () {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    console.log('++++++++++++++++++')
-    console.log('Welcome to Bamazon');
-    console.log('++++++++++++++++++')
+    console.log('     ++++++++++++++++++')
+    console.log('     Welcome to Bamazon');
+    console.log('     ++++++++++++++++++')
+  });
+  var table = new Table({
+    head: ['HEADER ONE', 'HEADER 2', 'HEADER THREE'],
+    colWidths: [100, 200],
+    colAligns: ["center", "left", "right"],
   });
 
-connection.end();
-}
+
+  table.push(
+    ['First value', 'Second value']
+    , ['First value', 'Second value']
+  )
+
+
+
+  connection.end();
+};
+
+
+
 
 displayItems();
 
@@ -46,11 +62,11 @@ displayItems();
 //         head: ['TH 1 label', 'TH 2 label']
 //       , colWidths: [100, 200]
 //     });
-     
-    
+
+
 //     table.push(
 //         ['First value', 'Second value']
 //       , ['First value', 'Second value']
 //     )};
-     
+
 //     display();
