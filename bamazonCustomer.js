2cda6105cb0e550f3f9226957f0e0c4e5e862ccc
Lrop <Lagropner04@gmail.com>
2019-08-05 14:59:47 -0600

    save

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,46 +21,17 @@ connection.connect(function (err) {
 });
 
 
-// var displayItems = function () {
-//   connection.query("SELECT * FROM products", function (err, res) {
-//     if (err) throw err;
-//     console.log('     ++++++++++++++++++')
-//     console.log('     Welcome to Bamazon');
-//     console.log('     ++++++++++++++++++')
-//   });
-//   var table = new Table({
-//     head: ['HEADER ONE', 'HEADER 2', 'HEADER THREE'],
-//     colWidths: [15, 50, 10],
-//     colAligns: ["center", "left", "right"],
-//   });
-//     for(var i = 0; i < res.length; i++){
-//     displayTable.push(
-//     [res[i].id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-//     );
-// }
-
-//     // for (var i; i < res.length; i++) {
-//     //   table.push([res[i].id, res[i].products_label, res[i].price]);
-//     // }
-//     console.log(table.toString());
-//     console.log('');
-
-
-//   connection.end();
-// };
-
-
 var displayProducts = function(){
 	var query = "Select * FROM products";
 	connection.query(query, function(err, res){
     if(err) throw err;
     console.log('                                  ++++++++++++++++++'.rainbow)
-    console.log('                                  Welcome to Bamazon'.white);
+    console.log('                                  Welcome to Bamazon'.red);
     console.log('                                  ++++++++++++++++++'.rainbow)
 
 		var displayTable = new Table ({
 			head: [" ID ".white.inverse, " Product Name ".white.inverse, " Catergory ".white.inverse, " Price ".white.inverse, " Quantity ".white.inverse],
-			colWidths: [10,25,25,10,14]
+			colWidths: [10,25,25,10,12]
     });
     
 		for(var i = 0; i < res.length; i++){
@@ -68,39 +39,43 @@ var displayProducts = function(){
 				[res[i].id,res[i].products_label, res[i].department_name, res[i].price, res[i].stock_quantity]
 				);
 		}
-		console.log(displayTable.toString());
+    console.log(displayTable.toString());
+    console.log('');
+    promtUser();
     
   });
   
-    connection.end();
+    // connection.end();
 }
 
 
 
 // displayProducts();
-// displayItems();
+
 
 
 var promtUser = function() {
   inquirer.prompt({
-    name:"productBuy",
+    name:"purchasedPRODUCT",
     type: "input",
     message: "Please enter the ID of the desired item you wish to buy!"
 
-  }).then(function(responseONE){
-    var selection = responseONE.productBuy;
-    connenction.query("SELECT * FROM products WHERE Id=?", selection, function(err, res){
+  }).then(function(responseFIRST){
+    var selection = responseFIRST.purchasedPRODUCT;
+    connection.query("SELECT * FROM products WHERE Id=?", selection, function(err, res){
+      // connection.end();
       if (err) throw err;
       if (res.length === 0){
         console.log("we dont got it");
-        
-
+        // promtUser();
+        // promtUser();
       }else {
         console.log("good");
-        promtUser();
+        
       };
+      connection.end();
     });
   });
 };
-displayProducts();
 // promtUser();
+displayProducts();
